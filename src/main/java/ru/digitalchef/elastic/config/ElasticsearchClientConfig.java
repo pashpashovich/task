ad9648b0be609779e78161b07ee2d13706ae8359
pashpashovich <pavel.kosovich2004@gmail.com>
2024-10-16 19:33:06 +0300

    init commit

---
+++ src/main/java/ru/digitalchef/elastic/config/ElasticsearchClientConfig.java
@@ -0,0 +1,22 @@
+package ru.digitalchef.elastic.config;
+
+import co.elastic.clients.elasticsearch.ElasticsearchClient;
+import co.elastic.clients.json.jackson.JacksonJsonpMapper;
+import co.elastic.clients.transport.rest_client.RestClientTransport;
+import org.apache.http.HttpHost;
+import org.elasticsearch.client.RestClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ElasticsearchClientConfig {
+
+    @Bean
+    public ElasticsearchClient elasticsearchClient() {
+        RestClient restClient = RestClient.builder(new HttpHost("localhost", 9200)).build();
+        RestClientTransport transport = new RestClientTransport(restClient, new JacksonJsonpMapper());
+
+        return new ElasticsearchClient(transport);
+    }
+}
+
